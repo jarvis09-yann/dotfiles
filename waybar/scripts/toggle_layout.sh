a3caceefc33cbb302ed467cec353d670dc9565c1
jarvis09 <72513680+jarvis09-yann@users.noreply.github.com>
2025-10-20 12:20:47 +0200

    Delete waybar directory

--- waybar/scripts/toggle_layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_layout=$(hyprctl devices -j | jq -r '.keyboards[] | select(.name=="at-translated-set-2-keyboard") | .active_keymap')
-
-if [ "$current_layout" = "French" ]; then
-    hyprctl switchxkblayout at-translated-set-2-keyboard 1
-else
-    hyprctl switchxkblayout at-translated-set-2-keyboard 0
-fi
